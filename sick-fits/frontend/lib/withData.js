d0bfc84b83c250416fdd4ed6d83e3a59c6ab29ed
Rafael Companhoni <companhoni@gmail.com>
2019-02-13 17:58:13 -0200

    Frontend: added Apollo Client to the project

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -2,6 +2,7 @@ import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
 
+// creates an Apollo Client which consumes the 'endpoint' GraphQL API
 function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
@@ -16,4 +17,5 @@ function createClient({ headers }) {
   });
 }
 
+// encapsulates with 'withApollo' so we can inject the client in the React app
 export default withApollo(createClient);
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,36 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
-  render() {
-    const { Component } = this.props;
+  // Next.js lifecycle method -- this makes every page to execute any GraphQL query contained in it (e.g. fetch list of items)
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    if(Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
 
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, apollo, pageProps } = this.props;
+
+    // wrap all pages of the app with the ApolloProvider so we can use the Apollo Client
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+// withData injects the actual Apollo Client to the React app (as a prop) which is used by the ApolloProvider (client prop, see above)
+export default withData(MyApp);
