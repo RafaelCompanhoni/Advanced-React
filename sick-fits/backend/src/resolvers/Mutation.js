fc8d31b0a9ea937b233067fd985c547bc91ff2b8
Rafael Companhoni <companhoni@gmail.com>
2019-03-19 23:13:33 -0300

    Delete an item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,28 +1,46 @@
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        // TODO: check if they are logged in
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
 
-        const item = await ctx.db.mutation.createItem({
-            data: {
-                ...args
-            },
-        }, info);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-        console.log(item);
+    console.log(item);
 
-        return item;
-    },
-    async updateItem(parent, args, ctx, info) {
-        // remove the id fom the item to be updated
-        const updates = { ...args };
-        delete updates.id;
+    return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    // remove the id fom the item to be updated
+    const updates = { ...args };
+    delete updates.id;
 
-        // the Prisma 'updateItem' receives the item to be updated and the 'where' clause to determine which item. Info is the query from the client-side, needed by Prisma
-        return ctx.db.mutation.updateItem({
-            data: updates,
-            where: { id: args.id }
-        }, info)
-    }
+    // the Prisma 'updateItem' receives the item to be updated and the 'where' clause to determine which item. Info is the query from the client-side, needed by Prisma
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{ id, title }`);
+
+    // check if they own the item or have the permissions
+    // TODO
+
+    // delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import styled from 'styled-components';
-import Item from './Item';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "./Item";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -18,36 +18,38 @@ const ALL_ITEMS_QUERY = gql`
 `;
 
 const Center = styled.div`
-    text-align: center;
+  text-align: center;
 `;
 
 const ItemList = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 60px;
-    max-width: ${props => props.theme.maxWidth};
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
 `;
 
 class Items extends Component {
-    render() {
-        return (
-            <Center>
-                <Query query={ALL_ITEMS_QUERY}>
-                    {({ data, error, loading }) => {
-                        if (loading) return <p>Loading...</p>
-                        if (error) return <p>Error: {error.messsage}</p>
+  render() {
+    return (
+      <Center>
+        <Query query={ALL_ITEMS_QUERY}>
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.messsage}</p>;
 
-                        return <ItemList>
-                            {data.items.map(item => {
-                                return <Item key={item.id} item={item} />
-                            })}
-                        </ItemList>
-                    }}
-                </Query>
-            </Center>
-        );
-    }
+            return (
+              <ItemList>
+                {data.items.map(item => {
+                  return <Item key={item.id} item={item} />;
+                })}
+              </ItemList>
+            );
+          }}
+        </Query>
+      </Center>
+    );
+  }
 }
 
 export default Items;
